c9409cd43542d2d573ab72682dde105fc15bd7c4
maihahoangthai <maihahoangthai@gmail.com>
2021-11-20 19:37:13 +0700

    First Project Push

---
+++ LightShopOnline/LightShopOnline/Models/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LightShopOnline.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Product_Id { get; set; }
+
+        public string Product_Name { get; set; }
+
+        public string url { get; set; }
+
+        public int Price { get; set; }
+
+        public string Warrant { get; set; }
+
+        public string Size { get; set; }
+
+        public string Color { get; set; }
+
+        public string Description { get; set; }
+
+        public string Band { get; set; }
+
+        public float Discount { get; set; }
+
+        public int isHidden { get; set; }
+
+        public string Picture1 { get; set; }
+
+        public string Picture2 { get; set; }
+
+        public string Picture3 { get; set; }
+
+        public string Picture4 { get; set; }
+    }
+}
---
+++ LightShopOnline/LightShopOnline/Models/UserTable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LightShopOnline.Models
+{
+    public class UserTable
+    {
+        [Key]
+        public int User_Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string ggAuthId { get; set; }
+
+        public string Role { get; set; }
+
+        public string Gmail { get; set; }
+    }
+}
