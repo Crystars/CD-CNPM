d771393dbc126611670c4c4bd89ee7b3e38984ff
Mai Ha Hoang Thai <maihahoangthai@gmail.com>
2022-01-07 23:56:07 +0700

    Adapt to the change of LightShopOnline.Models
    Fix Error related to Product Details page. In View/Product/Details, delete other picture rather than picture1. Delete some unused line that caused error in Repositories/ProductRes.

--- LightShopOnline/LightShopOnline/Repositories/ProductRes.cs
+++ LightShopOnline/LightShopOnline/Repositories/ProductRes.cs
@@ -34,9 +34,6 @@ namespace LightShopOnline.Repositories
                     sp.Discount = string.IsNullOrEmpty(dr["Discount"].ToString()) ? 0 : float.Parse(dr["Discount"].ToString());
                     sp.isHidden = string.IsNullOrEmpty(dr["isHidden"].ToString()) ? 0 : int.Parse(dr["isHidden"].ToString());
                     sp.Picture1 = dr["Picture1"].ToString();
-                    sp.Picture2 = dr["Picture2"].ToString();
-                    sp.Picture3 = dr["Picture3"].ToString();
-                    sp.Picture4 = dr["Picture4"].ToString();
 
                     lstResult.Add(sp);
                 }
@@ -51,7 +48,8 @@ namespace LightShopOnline.Repositories
             SQLCommand connection = new SQLCommand(ConstValue.ConnectionString);
             DataTable result = connection.Select("Product_GetDetailByURL", value);
             Product sp = new Product();
-            try {
+            try
+            {
                 if (connection.errorCode == 0 && result.Rows.Count > 0 && result.Rows.Count < 2)
                 {
                     foreach (DataRow dr in result.Rows)
@@ -68,17 +66,49 @@ namespace LightShopOnline.Repositories
                         sp.Discount = string.IsNullOrEmpty(dr["Discount"].ToString()) ? 0 : float.Parse(dr["Discount"].ToString());
                         sp.isHidden = string.IsNullOrEmpty(dr["isHidden"].ToString()) ? 0 : int.Parse(dr["isHidden"].ToString());
                         sp.Picture1 = dr["Picture1"].ToString();
-                        sp.Picture2 = dr["Picture2"].ToString();
-                        sp.Picture3 = dr["Picture3"].ToString();
-                        sp.Picture4 = dr["Picture4"].ToString();
                     }
                 }
 
-            } catch(Exception e) {
+            }
+            catch (Exception e)
+            {
                 Console.WriteLine(e.Message);
             }
 
             return sp;
         }
+
+        public static List<Product> GetCart()
+        {
+            object[] value = { };
+            SQLCommand connection = new SQLCommand(ConstValue.ConnectionString);
+            DataTable result = connection.Select("Product_GetAll", value);
+            List<Product> lstResult = new List<Product>();
+            if (connection.errorCode == 0 && result.Rows.Count > 0)
+            {
+                foreach (DataRow dr in result.Rows)
+                {
+                    Product sp = new Product();
+                    sp.Product_Id = string.IsNullOrEmpty(dr["Product_Id"].ToString()) ? 0 : int.Parse(dr["Product_Id"].ToString());
+                    sp.Product_Name = dr["Product_Name"].ToString();
+                    sp.url = dr["url"].ToString();
+                    sp.Price = string.IsNullOrEmpty(dr["Price"].ToString()) ? 0 : int.Parse(dr["Price"].ToString());
+                    sp.Warrant = dr["Warrant"].ToString();
+                    sp.Size = dr["Size"].ToString();
+                    sp.Color = dr["Color"].ToString();
+                    sp.Description = dr["Description"].ToString();
+                    sp.Brand = dr["Brand"].ToString();
+                    sp.Discount = string.IsNullOrEmpty(dr["Discount"].ToString()) ? 0 : float.Parse(dr["Discount"].ToString());
+                    sp.isHidden = string.IsNullOrEmpty(dr["isHidden"].ToString()) ? 0 : int.Parse(dr["isHidden"].ToString());
+                    sp.Picture1 = dr["Picture1"].ToString();
+
+                    lstResult.Add(sp);
+                }
+            }
+
+            return lstResult;
+        }
+
+
     }
 }
